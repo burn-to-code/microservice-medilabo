87a4b41fed9c182adc4c59dac092a4bd596dc1b6
Dylan Senasson <dylan.senasson@outlook.fr>
2025-10-01 10:19:48 +0200

    chore: add logs for patient CRUD operations (mask sensitive info)

--- patient/src/main/java/com/microservice_patient/patient/exception/GlobalExceptionHandler.java
+++ patient/src/main/java/com/microservice_patient/patient/exception/GlobalExceptionHandler.java
@@ -1,19 +1,24 @@
 package com.microservice_patient.patient.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(PatientNotFound.class)
     public Map<String, String> patientNotFound(PatientNotFound ex){
+        log.error("Patient not found", ex);
         return Map.of("error", ex.getMessage());
     }
+
     @ExceptionHandler(PatientConflictException.class)
     public Map<String, String> patientConflict(PatientConflictException ex){
+        log.error("Patient conflict", ex);
         return Map.of("error", ex.getMessage());
     }
 }
--- patient/src/main/java/com/microservice_patient/patient/service/PatientServiceImpl.java
+++ patient/src/main/java/com/microservice_patient/patient/service/PatientServiceImpl.java
@@ -5,42 +5,53 @@ import com.microservice_patient.patient.exception.PatientConflictException;
 import com.microservice_patient.patient.exception.PatientNotFound;
 import com.microservice_patient.patient.model.Patient;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class PatientServiceImpl implements PatientService{
     private final PatientRepository patientRepository;
 
     @Override
     public List<Patient> getAllPatients(){
+        log.debug("Getting all patients");
         return patientRepository.findAll();
     }
 
     @Override
     public Patient getPatientById(Long id){
-        return patientRepository.findById(id).orElseThrow(() ->
+        log.debug("Getting patient with id {}", id);
+        Patient patient = patientRepository.findById(id).orElseThrow(() ->
                  new PatientNotFound("Patient with id " + id + " not found")
         );
+        log.info("Found patient {}", patient.getFirstName()+" "+patient.getLastName());
+        return patient;
     }
 
     @Override
     public Patient savePatient(Patient patient){
+        log.debug("Saving patient {}", patient.getFirstName()+" "+patient.getLastName());
         if(patient.getId() != null){
             throw new PatientConflictException(
                     "Cannot create patient with predefined ID. Use PUT to update existing patient."
             );
         }
-        return patientRepository.save(patient);
+        Patient patientSave = patientRepository.save(patient);
+        log.info("Saved patient {}", patient.getFirstName()+" "+patient.getLastName());
+        return patientSave;
     }
 
     @Override
     public Patient updatePatient(Long id, Patient patientDetails){
+        log.debug("Updating patient with id {}", id);
         Patient existing = patientRepository.findById(id)
                 .orElseThrow(() -> new PatientNotFound("Patient with id " + id + " not found"));
 
+        log.debug("Existing patient {}", existing.getFirstName()+" "+existing.getLastName());
         existing.setFirstName(patientDetails.getFirstName());
         existing.setLastName(patientDetails.getLastName());
         existing.setDateOfBirth(patientDetails.getDateOfBirth());
@@ -48,15 +59,19 @@ public class PatientServiceImpl implements PatientService{
         existing.setAddress(patientDetails.getAddress());
         existing.setPhoneNumber(patientDetails.getPhoneNumber());
 
-        return patientRepository.save(existing);
+        Patient patientUpdate = patientRepository.save(existing);
+        log.info("Updated patient {}", patientUpdate.getFirstName()+" "+patientUpdate.getLastName());
+        return patientUpdate;
     }
 
     @Override
     public void deletePatientById(Long id){
+        log.debug("Deleting patient with id {}", id);
         if(!patientRepository.existsById(id)){
             throw new PatientNotFound("Patient with id " + id + " not found");
         }
         patientRepository.deleteById(id);
+        log.info("Deleted patient with id {}", id);
     }
 
 }
