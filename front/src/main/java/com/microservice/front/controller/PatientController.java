ed0e5d5319b178d75f9a4903cd2726225c0143b7
Dylan Senasson <dylan.senasson@outlook.fr>
2025-10-04 20:48:26 +0200

    feat(front): add button for the add page + add add.html

--- front/src/main/java/com/microservice/front/controller/PatientController.java
+++ front/src/main/java/com/microservice/front/controller/PatientController.java
@@ -2,9 +2,12 @@ package com.microservice.front.controller;
 
 import com.microservice.front.service.PatientService;
 import com.project.common.dto.PatientDTO;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,5 +27,21 @@ public class PatientController {
         List<PatientDTO> patients = patientService.getAllPatients();
         model.addAttribute("patients", patients);
         return "patient/list";
+    }
+
+    @GetMapping("/add")
+    public String addPatient(Model model){
+        model.addAttribute("patient", new PatientDTO());
+        return "patient/add";
+    }
+
+    @PostMapping("/save")
+    public String savePatient(@Valid PatientDTO patient, BindingResult result, Model model){
+        if(result.hasErrors()){
+            model.addAttribute("patient", patient);
+            return "patient/add";
+        }
+        patientService.savePatient(patient);
+        return "redirect:/medilabo/patient";
     }
 }
