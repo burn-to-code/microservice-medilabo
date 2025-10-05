2953a91bd864298f3683cc7e053596d1af9abb3b
Dylan Senasson <dylan.senasson@outlook.fr>
2025-10-05 12:00:18 +0200

    fix(patient) Ajout des response http correct en fonction de l'exception soulever

--- patient/src/main/java/com/microservice_patient/patient/exception/GlobalExceptionHandler.java
+++ patient/src/main/java/com/microservice_patient/patient/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.microservice_patient.patient.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
@@ -11,12 +13,14 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(PatientNotFound.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> patientNotFound(PatientNotFound ex){
         log.error("Patient not found", ex);
         return Map.of("error", ex.getMessage());
     }
 
     @ExceptionHandler(PatientConflictException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
     public Map<String, String> patientConflict(PatientConflictException ex){
         log.error("Patient conflict", ex);
         return Map.of("error", ex.getMessage());
