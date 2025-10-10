4225c76d97efc9722ff056f8a6379e0cba10f239
Dylan Senasson <dylan.senasson@outlook.fr>
2025-10-10 14:47:40 +0200

    Feat(Front/login) : Implémentation d'une page de login et implémentation du controller

---
+++ front/src/main/java/com/microservice/front/controller/LoginController.java
@@ -0,0 +1,39 @@
+package com.microservice.front.controller;
+
+import com.microservice.front.client.PatientClientInterface;
+import com.microservice.front.config.FeignExceptionConfig;
+import com.microservice.front.config.security.AuthSession;
+import feign.FeignException;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequiredArgsConstructor
+@Controller
+public class LoginController {
+
+    private final AuthSession authSession;
+    private final PatientClientInterface patientClientInterface;
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String tryLogin(@RequestParam String username, @RequestParam String password, Model model, HttpSession httpSession){
+        authSession.login(username, password);
+        try {
+            patientClientInterface.getAllPatients();
+            return "redirect:/patient";
+        } catch (FeignException.Unauthorized e) {
+            authSession.logout();
+            model.addAttribute("error", "erreur : login ou mot de passe incorrect");
+            return "login";
+        }
+    }
+}
