0439482b36f207aa1abe560a2aeb8de987434d3a
Dylan Senasson <dylan.senasson@outlook.fr>
2025-10-10 12:19:03 +0200

    feat(microservice): chagement des port entre le front et la gateway pour accéder au front via le 8080

--- gateway/src/main/java/com/microservice/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/microservice/gateway/config/SecurityConfig.java
@@ -2,52 +2,17 @@ package com.microservice.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-
-
-    @Bean
-    SecurityFilterChain apiSecurity(HttpSecurity http) throws Exception {
-        http
-                .securityMatcher("/patients/**")
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable);
-
-        return http.build();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/medilabo/**").authenticated()
-                        .anyRequest().permitAll()
-                )
-                .formLogin(form -> form
-                        .defaultSuccessUrl("/medilabo/patient", true)
-                        .permitAll()
-                )
-                .csrf(AbstractHttpConfigurer::disable)
-                .logout(LogoutConfigurer::permitAll);
-
-        return http.build();
-    }
 
 
     @Bean
