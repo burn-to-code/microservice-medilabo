a9552be2acbe9a1e070cd00adfaff9b6f78904e1
Dylan Senasson <dylan.senasson@outlook.fr>
2025-10-11 15:04:51 +0200

    fix(security/gatewway) : Passage de la sécurity en webFlux car la gateway est passé en WebFlux

--- gateway/src/main/java/com/microservice/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/microservice/gateway/config/SecurityConfig.java
@@ -3,23 +3,20 @@ package com.microservice.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig  {
 
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()
-                )
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
