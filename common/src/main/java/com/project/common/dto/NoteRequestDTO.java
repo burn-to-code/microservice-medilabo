5d8dd2a10fece4dac9370bb6c568693716f48e21
Dylan Senasson <dylan.senasson@outlook.fr>
2025-10-12 20:09:48 +0200

    feat(common) : Ajout de règle pour controller le DTO avec Jakarta

--- common/src/main/java/com/project/common/dto/NoteRequestDTO.java
+++ common/src/main/java/com/project/common/dto/NoteRequestDTO.java
@@ -1,7 +1,13 @@
 package com.project.common.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record NoteRequestDTO(
+        @NotNull(message = "Patient ID is required")
         Long patientID,
+        @NotBlank(message = "Patient name is required")
         String patientName,
+        @NotBlank(message = "Note is required")
         String note
 ) { }
