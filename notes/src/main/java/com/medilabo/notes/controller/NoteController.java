56802aec30948caf658afc80982ff5cbfb4aaec3
Dylan Senasson <dylan.senasson@outlook.fr>
2025-10-12 20:15:13 +0200

    feat(Notes) : Ajout du controller

---
+++ notes/src/main/java/com/medilabo/notes/controller/NoteController.java
@@ -0,0 +1,33 @@
+package com.medilabo.notes.controller;
+
+import com.medilabo.notes.service.NoteService;
+import com.project.common.dto.NoteRequestDTO;
+import com.project.common.dto.NoteResponseDTO;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/notes")
+@AllArgsConstructor
+public class NoteController {
+
+    private final NoteService noteService;
+
+    @GetMapping("/{patientId}")
+    public ResponseEntity<List<NoteResponseDTO>> getNotesByPatientId(@PathVariable Long patientId){
+        List<NoteResponseDTO> notes = noteService.getNoteAndDateByPatientId(patientId);
+        return ResponseEntity.ok(notes);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveNote(@RequestBody @Valid NoteRequestDTO note){
+        noteService.saveNote(note);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+}
