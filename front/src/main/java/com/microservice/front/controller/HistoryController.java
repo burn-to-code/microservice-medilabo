eb3a97f9a7f301f15d93a676324f1cd048737bca
Dylan Senasson <dylan.senasson@outlook.fr>
2025-10-16 16:37:56 +0200

    Fix:(front): Ajout des données existant immédiatemetn dans le model au lieu de passer par th.value

--- front/src/main/java/com/microservice/front/controller/HistoryController.java
+++ front/src/main/java/com/microservice/front/controller/HistoryController.java
@@ -3,11 +3,13 @@ package com.microservice.front.controller;
 import com.microservice.front.service.NoteService;
 import com.microservice.front.service.PatientService;
 import com.project.common.dto.NoteRequestDTO;
+import com.project.common.dto.PatientDTO;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -30,15 +32,21 @@ public class HistoryController {
         Assert.notNull(id, "PatientId must not be null");
         Assert.isTrue(id > 0, "PatientId must be greater than 0");
 
-        model.addAttribute("patient", patientService.getPatientById(id));
+        PatientDTO patient = patientService.getPatientById(id);
+
+        model.addAttribute("patient", patient);
         model.addAttribute("notes", noteService.getNoteAndDateByPatientId(id));
-        model.addAttribute("noteRequest", new NoteRequestDTO());
+        model.addAttribute("noteRequest", new NoteRequestDTO(patient.getId(), patient.getFirstName()));
 
         return "patient/history";
     }
 
     @PostMapping("/save")
-    public String saveNote(@Valid @ModelAttribute("noteRequest") NoteRequestDTO note){
+    public String saveNote(@Valid @ModelAttribute("noteRequest") NoteRequestDTO note, BindingResult result){
+        if(result.hasErrors()){
+            log.debug("Binding errors: {}", result.getAllErrors());
+            return "patient/history";
+        }
         log.debug("Saving note {}", note);
         noteService.saveNote(note);
 
