f30efeba85dffd5d08376672f1d1375323ba7db0
Dylan Senasson <dylan.senasson@outlook.fr>
2025-10-17 03:22:11 +0200

    Feat(Risk) : Configuration du Feign CLient sur l'application

--- risk/src/main/java/com/microservice/risk/RiskApplication.java
+++ risk/src/main/java/com/microservice/risk/RiskApplication.java
@@ -3,9 +3,11 @@ package com.microservice.risk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class RiskApplication {
 
     public static void main(String[] args) {
