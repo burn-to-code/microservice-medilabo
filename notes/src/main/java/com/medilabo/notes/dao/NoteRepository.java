8d64796b1752819e30bb3122f6475beccad1c37c
Dylan Senasson <dylan.senasson@outlook.fr>
2025-10-20 15:43:25 +0200

    Refactor(Notes) : Passage sur une query avec une interface pour la performance de récupération des notes.

--- notes/src/main/java/com/medilabo/notes/dao/NoteRepository.java
+++ notes/src/main/java/com/medilabo/notes/dao/NoteRepository.java
@@ -1,7 +1,9 @@
 package com.medilabo.notes.dao;
 
 import com.medilabo.notes.model.Note;
+import com.medilabo.notes.model.NoteProjection;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,7 @@ import java.util.List;
 @Repository
 public interface NoteRepository extends MongoRepository<Note, String> {
 
-    List<Note> findByPatientIdOrderByDateDesc(Long patientId);
+    @Query(value = "{'patientId' : ?0}", fields = "{'patId' : 1, 'date' : 1, 'note' : 1}", sort = "{'date' : -1}")
+    List<NoteProjection> findNoteAndDateByPatientIdOrderByDateDesc(Long patientId);
 }
 
---
+++ notes/src/main/java/com/medilabo/notes/model/NoteProjection.java
@@ -0,0 +1,8 @@
+package com.medilabo.notes.model;
+
+import java.time.LocalDate;
+
+public interface NoteProjection {
+    String getNote();
+    LocalDate getDate();
+}
